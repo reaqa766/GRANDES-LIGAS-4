3ab79b87fbf209f0779a4221334944fa49499ec9
rafaelQ <reaqa766@gmail.com>
2018-06-05 08:59:45 -0400

    primer push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CuadroComponent } from './cuadro/cuadro.component';
+import { JardinesComponent } from './jardines/jardines.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { PitchersComponent } from './pitchers/pitchers.component';
+import { RegistreseComponent } from './registrese/registrese.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent,
+   
+  },
+  
+  
+  {
+    path:'cuadro',
+    component: CuadroComponent,
+   
+  },
+
+  {
+    path: 'jardines',
+    component: JardinesComponent,
+
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent,
+  },
+
+  {
+    path: 'noticias',
+    component: NoticiasComponent,
+  },
+
+  {
+    path: 'pitchers',
+    component: PitchersComponent,
+  },
+
+  {
+    path: 'registrese',
+    component: RegistreseComponent,
+  },
+  {
+    path: 'ingresar',
+    component: IngresarComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/nosostros/nosostros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nosostros',
+  templateUrl: './nosostros.component.html',
+  styleUrls: ['./nosostros.component.css']
+})
+export class NosostrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
